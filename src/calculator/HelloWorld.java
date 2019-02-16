fa79faa643599526cf4391c71175ab5d958ea8cb
Oshada Maheeshan <oshadamahee@gmail.com>
2019-02-16 17:51:04 +0530

    modify welcome message

--- src/calculator/HelloWorld.java
+++ src/calculator/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
     @SuppressWarnings("unused")
 	public static void main(String[] args) {
     	
-    	System.out.println("Welcome to Java World.");
+    	System.out.println("Welcome to fucking Java World.");
         
     }
 }
